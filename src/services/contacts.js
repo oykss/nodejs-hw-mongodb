d2b5185f7a1d212898c90f986b37254c28c02649
Pavlo-Chernicenko <pwned7067@gmail.com>
2025-02-12 22:40:17 +0200

    Formatted

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -2,5 +2,4 @@ import { ContactsCollection } from '../db/models/contacts.js';
 
 export const getAllContacts = async () => await ContactsCollection.find();
 
-export const getContactById = async (contactId) =>
-  await ContactsCollection.findById(contactId);
+export const getContactById = async (contactId) => await ContactsCollection.findById(contactId);
