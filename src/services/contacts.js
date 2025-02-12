04eb0261b4a72f9f675267d42d32ab2bc2859478
Pavlo-Chernicenko <pwned7067@gmail.com>
2025-02-12 17:45:54 +0200

    Added an initial connection to MongoDB and configured the Express server with routes for contact management

---
+++ src/services/contacts.js
@@ -0,0 +1,6 @@
+import { ContactsCollection } from '../db/models/contacts.js';
+
+export const getAllContacts = async () => await ContactsCollection.find();
+
+export const getContactById = async (contactId) =>
+  await ContactsCollection.findById(contactId);
