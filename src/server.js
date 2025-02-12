61573d96f56bc891f879ea0bed60fc1d47c7edce
Pavlo-Chernicenko <pwned7067@gmail.com>
2025-02-12 22:39:34 +0200

    Fixed status formatting in replies

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ export const setupServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
-      status: '200',
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -42,7 +42,7 @@ export const setupServer = () => {
     }
 
     res.status(200).json({
-      status: '200',
+      status: 200,
       message: 'Successfully found contact with id {contactId}!',
       data: contact,
     });
