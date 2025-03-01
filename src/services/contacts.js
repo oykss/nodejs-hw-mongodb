779bdd4b945f502f21df63b9c3a986728d980210
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-01 21:52:49 +0200

    Added controllers and routes for contact management

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,5 +1,31 @@
-import { ContactsCollection } from '../db/models/contacts.js';
+import { ContactsCollection } from "../db/models/contacts.js";
 
 export const getAllContacts = async () => await ContactsCollection.find();
 
-export const getContactById = async (contactId) => await ContactsCollection.findById(contactId);
+export const getContactById = async (contactId) =>
+  await ContactsCollection.findById(contactId);
+
+export const createContact = async (payload) =>
+  await ContactsCollection.create(payload);
+
+export const updateContact = async (contactId, payload, options = {}) => {
+  const result = await ContactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    }
+  );
+
+  if (!result || !result.value) return null;
+
+  return {
+    contact: result.value,
+    isNew: Boolean(result?.lastErrorObject?.upserted),
+  };
+};
+
+export const deleteContact = async (contactId) =>
+  await ContactsCollection.findOneAndDelete({ _id: contactId });
