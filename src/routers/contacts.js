0394495af12f29df5f3dfd03e257952bc4f4d11a
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-09 22:17:09 +0200

    Added pagination for contact retrieval and improved code formatting

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,38 +1,38 @@
-import { Router } from "express";
+import { Router } from 'express';
 import {
   createContactController,
   deleteContactController,
   getContactByIdController,
   getContactsController,
   updateContactController,
-} from "../controllers/contacts.js";
-import { isValidId } from "../middlewares/isValidId.js";
-import { jsonParser } from "../middlewares/jsonParser.js";
-import { validateBody } from "../middlewares/validateBody.js";
-import { ctrlWrapper } from "../utils/ctrlWrapper.js";
-import { createContactSchema } from "../validation/contacts.js";
+} from '../controllers/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import { jsonParser } from '../middlewares/jsonParser.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { createContactSchema } from '../validation/contacts.js';
 
 const router = Router();
 
-router.get("/", ctrlWrapper(getContactsController));
+router.get('/', jsonParser, ctrlWrapper(getContactsController));
 
-router.get("/:contactId", isValidId, ctrlWrapper(getContactByIdController));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 
 router.post(
-  "/",
+  '/',
   jsonParser,
   validateBody(createContactSchema),
   ctrlWrapper(createContactController)
 );
 
 router.patch(
-  "/:contactId",
+  '/:contactId',
   jsonParser,
   isValidId,
   validateBody(createContactSchema),
   ctrlWrapper(updateContactController)
 );
 
-router.delete("/:contactId", isValidId, ctrlWrapper(deleteContactController));
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
 export default router;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,11 +1,31 @@
-import { ContactsCollection } from "../db/models/contacts.js";
+import { ContactsCollection } from '../db/models/contacts.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = async () => await ContactsCollection.find();
+export const getAllContacts = async ({ page, perPage }) => {
+  console.log(page, perPage);
 
-export const getContactById = async (contactId) =>
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = ContactsCollection.find();
+  const contactsCount = await ContactsCollection.find()
+    .merge(contactsQuery)
+    .countDocuments();
+
+  const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
+};
+
+export const getContactById = async contactId =>
   await ContactsCollection.findById(contactId);
 
-export const createContact = async (payload) =>
+export const createContact = async payload =>
   await ContactsCollection.create(payload);
 
 export const updateContact = async (contactId, payload, options = {}) => {
@@ -27,5 +47,5 @@ export const updateContact = async (contactId, payload, options = {}) => {
   };
 };
 
-export const deleteContact = async (contactId) =>
+export const deleteContact = async contactId =>
   await ContactsCollection.findOneAndDelete({ _id: contactId });
