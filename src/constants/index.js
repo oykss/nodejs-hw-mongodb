a2e18250a02219dca53e9459a87d86115970cd59
Pavlo Chernichenko <pwned7067@gmail.com>
2025-03-17 09:56:41 +0200

    Added constants for regular email expression and time intervals

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,3 +9,9 @@ export const CONTACT_KEYS = Object.keys(ContactsCollection.schema.paths);
 
 export const CONTACT_TYPES =
   ContactsCollection.schema.path('contactType').enumValues;
+
+export const EMAIL_REGEX = /@/;
+
+export const FIFTEEN_MINUTES = 15 * 60 * 1000;
+
+export const ONE_DAY = 24 * 60 * 60 * 1000;
